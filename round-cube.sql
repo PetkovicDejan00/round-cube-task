d662e2da6b96295065cf9e82fc3dcc2412dc0b10
Dejan Petkovic <104632857+PetkovicDejan00@users.noreply.github.com>
2023-05-30 12:37:43 +0200

    Add files via upload
    mysql workbench schema with table

---
+++ round-cube.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `round-cube` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
